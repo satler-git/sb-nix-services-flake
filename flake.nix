32af15da98ecbc19a84965bbc8c9294c9334f8cc
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-14 19:50:10 +0900

    feat: use flake-parts and add it in inputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,38 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      # imports = [
+      #   inputs.process-compose-flake.flakeModule
+      # ];
+      perSystem =
+        {
+          self',
+          pkgs,
+          config,
+          lib,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ ];
+            buildInputs = with pkgs; [ hello ];
+          };
+        };
+    };
 }
